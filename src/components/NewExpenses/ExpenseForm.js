7183c2a268d57149054a690f8080d96fca6c3652
Felipe Teixeira Schefer <felipe_official@outlook.com>
2021-05-30 00:27:43 -0300

    class 58
    adding two-way binding

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,26 +1,106 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  /** 
+   *  There are 2 ways of setting values to the inputs in the form one is like this code below
+   *  which starts with an empty string and then that is involked by a handler function which 
+   *  updates the setVariable 
+   * */
+  const [enteredTitle,  setEnteredTitle ] = useState('')
+  const [enteredAmount, setEnteredAmount] = useState('')
+  const [enteredDate,   setEnteredData  ] = useState('')
+
+  /**
+   * Similarly to the code above this desconstructed array isn't receiving an empty string but an object
+   * which contains empty string variables 
+   */
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle:  '',
+  //   enteredAmount: '',
+  //   enteredDate:   ''
+  // })
+
   const titleChangeHandler = (event) =>{
-    console.log(event.target.value)
+    setEnteredTitle(event.target.value) // this is related to the code in the line 11
+
+    // setUserInput({                      // in this way, you sometimes got a error because it depends of the userInput which sometimes can be not up to date  
+    //   ...userInput,
+    //   enteredTitle: event.target.value
+    // })
+
+    // setUserInput((prevState) =>{            // in the other hand this piece of code doesn't have a problem being not up to date
+    //   return { 
+    //     ...prevState,
+    //     enteredTitle: event.target.value
+    //   }
+    // })
   }
 
+  const amountChangeHandler = event =>{
+    setEnteredAmount(event.target.value) // this is related to the code in the line 12
+   
+    // setUserInput({                       // in this way, you sometimes got a error because it depends of the userInput which sometimes can be not up to date  
+    //   ...userInput,
+    //   enteredAmount: event.target.value
+    // })
+
+    // setUserInput((prevState) =>{            // in the other hand this piece of code doesn't have a problem being not up to date
+    //   return {  
+    //     ...prevState,
+    //     enteredAmount: event.target.value 
+    //   }
+    // })
+  }
+
+  const dateChangeHandler = event =>{
+    setEnteredData(event.target.value) // this is related to the code in the line 13
+
+    // setUserInput({                     // in this way, you sometimes got a error because it depends of the userInput which sometimes can be not up to date  
+    //   ...userInput,
+    //   setUserInput: event.target.value
+    // })
+  }
+
+  const submitHandler = (event) =>{
+    event.preventDefault()
+
+    const expenseData ={
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+    console.log(expenseData)
+    setEnteredTitle('')
+    setEnteredAmount('')
+    setEnteredData('')
+  }
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler}/>
+          <input 
+            type='text'
+            value={enteredTitle}
+            onChange={titleChangeHandler}/>
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input 
+            type='number' 
+            value={enteredAmount}
+            min='0.01' step='0.01' 
+            onChange={amountChangeHandler}/>
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' />
+          <input 
+            type='date'
+            value={enteredDate}
+            min='2019-01-01' max='2022-12-31'
+            onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className='new-expense__actions'>
